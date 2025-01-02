6b73aac8ae5dce99727b43b771fa3b60030ec3be
RafaelPil <rafik.pilumyan@gmail.com>
2025-01-02 22:47:09 +0200

    LangQuest Expo Project

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,23 @@
-import { Stack, Link } from 'expo-router';
-
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
+import { Image, StatusBar, Text, View } from 'react-native';
 
 export default function Home() {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
-    </>
+    <View className="flex flex-1 items-center justify-center p-3">
+      <StatusBar animated barStyle={'light-content'} />
+      <Text className="items-stretch font-bold">fafa</Text>
+
+      <View className="w-[100%] flex-1 flex-row flex-wrap content-between justify-between">
+        <View className="h-[48%] w-[48%] items-center rounded-md rounded-b-lg border-2 border-gray-200 p-2">
+          <Image
+            source={{
+              uri: 'https://fsmd-assets.s3.eu-west-1.amazonaws.com/duolingo/images/glass.png',
+            }}
+            className="w-[100%] flex-1"
+            resizeMode="contain"
+          />
+          <Text>Glass</Text>
+        </View>
+      </View>
+    </View>
   );
 }
